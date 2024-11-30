47850b01fceab4ee33f32e7b41b9df30f43bb0bb
Piotr Pelc <piotrpelc83@gmail.com>
2024-11-30 16:28:32 +0100

    Add data fetching from server

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
+import { settings } from './settings.js';
+
 const app = {
   init: function(){
-    console.log('works');
+    this.initData();
+  },
+
+  initData:function(){
+    this.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((parsedResponse) => {
+        this.data.products = parsedResponse;
+      })
+      .then(() => {
+        console.log(this.data.products);
+      });
   }
 };
 
